c767553adf13ff43be921ec2fdb39685a726694f
khadim Diagne <khadim.diagne@etu.univ-st-etienne.fr>
2026-05-08 11:24:37 +0200

    Update LogoKN.jsx

--- src/components/LogoKN.jsx
+++ src/components/LogoKN.jsx
@@ -2,7 +2,7 @@ export default function LogoKN() {
   return (
     <div className="logo-kn glass">
       <span className="kn-letter k">K</span>
-      <span className="kn-letter n">N</span>
+      <span className="kn-letter d">D</span>
     </div>
   );
 }
